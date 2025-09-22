0a7604178e5c043d82fb808a9cf55b79f380c731
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-23 01:49:17 +0900

    Update build_and_package.sh

--- build_and_package.sh
+++ build_and_package.sh
@@ -6,7 +6,7 @@ echo "[*] Building UltimatePixelmonRating v1.5.6 (Java 8 / Spigot 1.16.5)"
 mvn -q -DskipTests package
 OUT="$DIR/out"
 mkdir -p "$OUT"
-cp target/UltimatePixelmonRating-1.5.6.jar "$OUT/"
+cp target/UltimatePixelmonRating-1.5.8.jar "$OUT/"
 cp src/main/resources/config.yml "$OUT/"
 cp src/main/resources/rewards.yml "$OUT/"
 cp src/main/resources/arenas.yml "$OUT/"
