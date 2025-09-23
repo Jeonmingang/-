2ebc755712128acf8ad1ed90f5638ef9759d78ec
SAMKOREA <123339750+Jeonmingang@users.noreply.github.com>
2025-09-23 11:54:42 +0900

    Delete src/main directory

--- src/main/java/com/minkang/ultimate/pixelrating/ProtocolHook.java
+++
@@ -1,6 +0,0 @@
-package com.minkang.ultimate.pixelrating;
-import org.bukkit.Bukkit; import org.bukkit.plugin.Plugin;
-public class ProtocolHook { public static void safeInit(UltimatePixelmonRatingPlugin plugin){
-  try{ Plugin p=Bukkit.getPluginManager().getPlugin("ProtocolLib"); if(p==null || !p.isEnabled()) return; plugin.getLogger().info("[UPR] ProtocolLib detected."); }
-  catch(Throwable ignored){}
-}}
